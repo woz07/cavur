cf14d55b0eb4a39972232c0236a1aed9242bcb8f
woz07 <muhsameer9@gmail.com>
2025-02-20 20:56:13 +0000

    ADD: constructor and comments

--- src/main/java/github/woz07/cavur/entities/Entity.java
+++ src/main/java/github/woz07/cavur/entities/Entity.java
@@ -1,15 +1,31 @@
 package github.woz07.cavur.entities;
 
+import com.raylib.Raylib;
+
 /**
  * Entity.java
  * The abstract class that stores data for all moveable objects in game
  */
 public abstract class Entity {
-    int width;  // The width of entity
-    int height; // The height of entity
-    int posx;   // The x position
-    int posy;   // The y position
-    int velx;     // The velocity x
-    int vely;     // The velocity y
-    int speed;  // The speed of entity
+    public int width;           // The width of entity
+    public int height;          // The height of entity
+    public int posx;            // The x position
+    public int posy;            // The y position
+    public int velx;            // The velocity x
+    public int vely;            // The velocity y
+    public int speed;           // The speed of entity
+    public Raylib.Color color;  // The color of entity
+
+    public Entity() {}
+
+    public Entity(int width, int height, int posx, int posy, int velx, int vely, int speed, Raylib.Color color) {
+        this.width  = width;
+        this.height = height;
+        this.posx   = posx;
+        this.posy   = posy;
+        this.velx   = velx;
+        this.vely   = vely;
+        this.speed  = speed;
+        this.color  = color;
+    }
 }
