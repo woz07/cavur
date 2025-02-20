85ae8c25c7a492e380dcc6e45cc08453630cd8f7
woz07 <muhsameer9@gmail.com>
2025-02-20 20:48:46 +0000

    ADD: abstract entity class

--- src/main/java/github/woz07/cavur/entities/Entity.java
+++ src/main/java/github/woz07/cavur/entities/Entity.java
@@ -1,8 +1,15 @@
 package github.woz07.cavur.entities;
 
 /**
- * 
+ * Entity.java
+ * The abstract class that stores data for all moveable objects in game
  */
 public abstract class Entity {
-
+    int width;  // The width of entity
+    int height; // The height of entity
+    int posx;   // The x position
+    int posy;   // The y position
+    int velx;     // The velocity x
+    int vely;     // The velocity y
+    int speed;  // The speed of entity
 }
